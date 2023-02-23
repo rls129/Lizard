74cf421ef9d5c422ec186b388e4d38c96fb14af2
Okabekiyouma <okiyouma00ichi@gmail.com>
2023-02-23 12:37:46 +0545

    Entity Error Handling

--- entity.py
+++ entity.py
@@ -1,7 +1,7 @@
 from lark import Token, Tree
 from enum import Enum
 from typing import List
-
+import error
 
 class Port:
     def __init__(self, name, dirn, ptype):
@@ -23,15 +23,25 @@ def get_entites(ast: Tree) -> List[Entity]:
     for tokens in ast.children:
         if tokens.data.value == "entity":
             if len(tokens.children) > 2:
-                assert (
+                if (
                     tokens.children[0].children[0].value
-                    == tokens.children[-1].children[0].value
-                )
+                    != tokens.children[-1].children[0].value
+                ):
+                    error.push_error(tokens.children[-1].children[0].line, tokens.children[-1].children[0].column, 
+                                     f"Unmatched Closing Identifier {tokens.children[0].children[0].value}")
+                    continue
             ports = get_ports(tokens)
+            entity_name = tokens.children[0].children[0]
 
-            entities.append(
-                Entity(tokens.children[0].children[0].value, ports)
-            )
+            unique = True
+            for e in entities:
+                if e.name == entity_name.value:
+                    error.push_error(entity_name.line, entity_name.column, f"Redefination of entity {e.name}")
+                    unique = False
+            if unique:
+                entities.append(
+                    Entity(tokens.children[0].children[0].value, ports)
+                )
     return entities
 
 
--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from vhdl_ast import parse, Tree, Token
 from utils import treerecursive
 from entity import get_entites, print_entities
 from architecture import get_architecture, print_architecture
+import error
 
 def main():
     ast: Tree = parse("vhdl.lark", "main.vhdl")
     print(ast.pretty())
     entities = get_entites(ast)
     print_entities(entities)
-    architectures = get_architecture(ast)
+    print(error.errno)
+    # architectures = get_architecture(ast, entities)
     # print_architecture(architectures)
 
 main()
