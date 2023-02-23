47b9a248d0b2d3cb440ada34db387a2e339a93b2
Okabekiyouma <okiyouma00ichi@gmail.com>
2023-02-23 11:48:23 +0545

    Restructure entities

--- entity.py
+++ entity.py
@@ -12,12 +12,10 @@ class Port:
 
 class Entity:
     def __init__(
-        self, name: str, iports: List[Port], oports: List[Port], ioports: List[Port]
+        self, name: str, ports: List[Port]
     ):
         self.name = name
-        self.iports = iports
-        self.oports = oports
-        self.ioports = ioports
+        self.ports = ports
 
 
 def get_entites(ast: Tree) -> List[Entity]:
@@ -29,38 +27,32 @@ def get_entites(ast: Tree) -> List[Entity]:
                     tokens.children[0].children[0].value
                     == tokens.children[-1].children[0].value
                 )
-            iports = get_ports(tokens, "in")
-            oports = get_ports(tokens, "out")
-            ioports = get_ports(tokens, "inout")
+            ports = get_ports(tokens)
+
             entities.append(
-                Entity(tokens.children[0].children[0].value, iports, oports, ioports)
+                Entity(tokens.children[0].children[0].value, ports)
             )
     return entities
 
 
-def get_ports(entity: Tree, typ: str):
+def get_ports(entity: Tree):
     ports = []
     for prop in entity.children:
         if prop.data.value == "portdecl":
             for port in prop.children:
                 assert port.data.value == "port"
-                if port.children[1].value == typ:
-                    ports.append(
-                        Port(
-                            port.children[0].children[0].value,
-                            port.children[1].value,
-                            port.children[2].value,
-                        )
+                ports.append(
+                    Port(
+                        port.children[0].children[0].value,
+                        port.children[1].value,
+                        port.children[2].value,
                     )
+                )
     return ports
 
 
 def print_entities(ent: List[Entity]):
     for i in ent:
         print(i.name)
-        for ii in i.iports:
-            print("IPort", ii.name)
-        for ii in i.oports:
-            print("Oport", ii.name)
-        for ii in i.ioports:
-            print("IOport", ii.name)
+        for ii in i.ports:
+            print(ii.type, ii.dirn, ii.name)
